a11c189dfc8566a36ead793c411ba684a8f6a2d4
Atsu77 <kamnknon@gmail.com>
2022-01-17 19:51:57 +0900

    remove mail auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:show] do
-        resource :plans, only: [:show, :create, :update]
-      end
     end
     mount_devise_token_auth_for 'User', at: 'auth', controllers: {
       registrations: 'api/auth/registrations'
